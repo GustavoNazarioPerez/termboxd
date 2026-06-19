1c18dbc98d4d897b981cf3fe69031d762d69c598
Gustavo Nazario Perez <gustavonazario27@gmail.com>
2026-06-19 16:44:30 -0400

    Add image visualization
    Includes "La La Land" poster as an example.

--- src/main.rs
+++ src/main.rs
@@ -2,26 +2,17 @@ mod movies;
 mod parser;
 
 fn main() {
-    // Hardcoding this for now bc who cares (if you're reading this follow me)
+    // // Hardcoding this for now bc who cares (if you're reading this follow me)
     let username = String::from("funnyguygus");
 
     let diary = parser::create_diary(&username);
-    println!("Diary");
-    println!("============");
     for mov in diary {
-        let title = mov.name;
-        let rating = mov.rating.unwrap_or_default();
-        let review = mov.review.unwrap_or_default();
-        println!("{title}: {rating}");
-        println!("{review}");
+        mov.visualize_poster();
     }
 
-    // construct watchlist: list of unseen movies
-    println!("\nWatchlist");
-    println!("============");
+    // // construct watchlist: list of unseen movies
     let watchlist = parser::create_watchlist(&username);
     for mov in watchlist {
-        let title = mov.name;
-        println!("{title}");
+        mov.visualize_poster();
     }
 }
--- src/movies.rs
+++ src/movies.rs
@@ -1,3 +1,7 @@
+use env;
+use image::{DynamicImage, Pixel, imageops::Triangle};
+use tmdb_client::apis::client::APIClient;
+
 #[derive(Default, serde::Deserialize)]
 #[serde(default, rename_all = "PascalCase")]
 pub struct Movie {
@@ -8,4 +12,55 @@ pub struct Movie {
     pub uri: String,
     pub rating: Option<f32>,
     pub review: Option<String>,
+}
+
+impl Movie {
+    fn get_poster_path(&self) -> String {
+        let tmdb_api_key = env::var("TMDB_API_KEY").expect("ERROR: TMDB API key failure");
+        let client = APIClient::new_with_api_key(tmdb_api_key);
+        let res = client
+            .search_api()
+            .get_search_movie_paginated(&self.name, Some(self.year), None, None, None, None, None)
+            .expect("ERROR: Failed to search movie with TMDB");
+        let results = res.results.unwrap_or_default();
+        let first = &results[0];
+        let path = &first.poster_path;
+        match path {
+            Some(value) => value.to_string(),
+            None => String::from(""),
+        }
+    }
+
+    fn get_poster(&self) -> DynamicImage {
+        let poster_path = self.get_poster_path();
+        let url = format!("https://image.tmdb.org/t/p/original{}", poster_path);
+        let response = reqwest::blocking::get(url);
+        let bytes = response
+            .expect("ERROR: Failed to fetch poster from IMDB")
+            .bytes()
+            .expect("ERROR: Failed decoding poster image");
+        image::load_from_memory(&bytes).expect("ERROR: Failed to load image from memory")
+    }
+
+    pub fn visualize_poster(&self) {
+        let img = self.get_poster();
+        let rgb = img.resize(48, 48, Triangle).into_rgba8();
+        let mut rows = rgb.rows();
+        while let Some(top_row) = rows.next() {
+            let merged = top_row.zip(rows.next().expect("Error"));
+            for (top, bottom) in merged {
+                let fore = top.channels();
+                let (fore_r, fore_g, fore_b) = (fore[0], fore[1], fore[2]);
+                let back = bottom.channels();
+                let (back_r, back_g, back_b) = (back[0], back[1], back[2]);
+                print!(
+                    "\x1b[38;2;{};{};{}m\x1b[48;2;{};{};{}m▀",
+                    fore_r, fore_g, fore_b, back_r, back_g, back_b
+                );
+                print!("\x1b[0m",);
+            }
+
+            println!("");
+        }
+    }
 }
